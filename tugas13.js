0e2c3cc53a65372160abd7776902744bbf3e09b6
s-prianto <prianto2796@gmail.com>
2020-02-12 06:30:55 +0700

    Tugas 13 JS

---
+++ tugas13.js
@@ -0,0 +1,9 @@
+function panggilnamabuah(){
+	var namabuah = ["Pisang","Jeruk"];
+	console.log(namabuah);
+
+	namabuah.unshift("Apel","Mangga");
+	return namabuah
+}
+
+console.log(panggilnamabuah());
